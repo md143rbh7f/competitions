88d7438b5e80ca922a402930e97b70208c3d531c
M <md143rbh7f@gmail.com>
2017-10-24 17:13:14 -0700

    Updated "Balls" from Greater New York 2009
    By inspecting the function and observing that the optimal floors are
    ascending, we can reduce the runtime of the dynamic program from
    O(B * M^2) to O(B * M).

--- acm-icpc/greater-new-york/09/Balls.java
+++ acm-icpc/greater-new-york/09/Balls.java
@@ -1,25 +1,18 @@
 import java.util.*;
 
-public class Balls
-{
-	public static void main( String[] args )
-	{
-		int B = 51, F = 1001, dp[][] = new int[B][F];
-		for( int f = 1; f < F; f++ ) dp[1][f] = f;
-		for( int b = 2; b < B; b++ ) for( int f = 1; f < F; f++ )
-		{
-			int del = 1 << 10, best = 0;
-			while( del != 0 )
-			{
-				int n = best + del;
-				if( n < f && dp[b-1][n-1] < dp[b][f-n] ) best += del;
-				del >>= 1;
-			}
-			dp[b][f] = 1 + Math.min( dp[b-1][best], dp[b][f-best] );
+public class Balls {
+	static int N = 51, M = 1001, best[][] = new int[N][M];
+
+	public static void main(String[] args) {
+		for (int j = 1; j < M; j++) best[1][j] = j;
+		for (int i = 2; i < N; i++) for (int j = 1, k = 1; j < M; j++) {
+			while (k < j && best[i - 1][k - 1] < best[i][j - k]) k++;
+			best[i][j] = 1 + best[i - 1][k - 1];
 		}
 
-		Scanner in = new Scanner( System.in );
+		Scanner in = new Scanner(System.in);
 		int c = in.nextInt();
-		for( int i = 1; i <= c; i++ ) System.out.println( in.nextInt() + " " + dp[in.nextInt()][in.nextInt()] );
+		for (int i = 0; i < c; i++)
+			System.out.println(in.nextInt() + " " + best[in.nextInt()][in.nextInt()]);
 	}
 }
