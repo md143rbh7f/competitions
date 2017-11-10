cdff14d27931cf3003c1481d56878080731101c9
M <md143rbh7f@gmail.com>
2017-11-10 10:15:50 -0800

    Updated FibonacciXor from SRM 622
    Also added memoisation decorator and recursion limit increase to the
    base Python template.

--- PRE/base.py
+++ PRE/base.py
@@ -4,12 +4,18 @@ import heapq
 from itertools import *
 from math import *
 #from functools import *
-## if t!
 import sys
+## if t!
 
 read_line = lambda: [int(i) for i in input().split()]
 ## endif
 INF = float('inf')
+
+sys.setrecursionlimit(1000)
+
+def memo(f):
+    m = {}
+    return lambda *a: m[a] if a in m else m.setdefault(a, f(*a))
 
 ## if t
 class $NAME(object):
--- topcoder/srm/622/FibonacciXor.py
+++ topcoder/srm/622/FibonacciXor.py
@@ -1,14 +1,23 @@
+def memo(f):
+    m = {}
+    return lambda *a: m[a] if a in m else m.setdefault(a, f(*a))
+
+
 fib = [1, 2]
 while fib[-1] + fib[-2] <= 1e15:
     fib.append(fib[-1] + fib[-2])
 
-mem = {1: 0, 2: 1}
-def f(n):
-    if n not in mem:
-        i = max(i for i, x in enumerate(fib) if x < n)
-        mem[n] = (((n ^ fib[i]) & 1) << i) ^ f(n - fib[i]) ^ f(fib[i])
-    return mem[n]
+
+@memo
+def h(n):
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    i, x = max((i, x) for i, x in enumerate(fib) if x < n)
+    return (((n - x) % 2) << i) ^ h(n - x) ^ h(x)
+
 
 class FibonacciXor(object):
     def find(self, a, b):
-        return (f(b + 1) ^ f(a)) % 1000000007
+        return (h(b + 1) ^ h(a)) % 1000000007
